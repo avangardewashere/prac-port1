4cae0a7dd7b71501186d9cbccc20cf0453b12c60
avangardewashere <avelpanaligan@gmail.com>
2026-01-04 21:37:57 +0800

    Add changes in service summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import ServiceSummary from './sections/ServiceSummary'
 
 function App() {
   return (
     <div className="relative w-screen min-h-screen overflow-x-auto ">
       <Navbar/>
       <Hero/>
+      <ServiceSummary/>
       <section id='home' className='min-h-screen '/>
      
     </div>
--- src/sections/ServiceSummary.tsx
+++ src/sections/ServiceSummary.tsx
@@ -1,17 +1,23 @@
 const ServiceSummary = () => {
     return (
-        <section className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive ">
+        <section className="mt-60 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive ">
             <div id="title-service-1">
                 <p>Architecture</p>
             </div>
             <div id="title-service-2" className="flex items-center justify-center gap-3 translate-x-16">
-                <p className="font-normal">Development</p>
-                <div className="w-10 h-1 md:w-32 bg-gold">
-                    <p>Deployment</p></div>
+                <p className="font-semibold">Development</p>
+                <div className="w-10 h-1 md:w-32 bg-gold"/>
+                    <p>Deployment</p> 
             </div>
-            <div className="" id="title-service-3">
-                <p className="flex-items-center justify-center gap-3 -translate-x-48">
-                </p>
+            <div className="flex items-center justify-center gap-3 -translate-x-48" id="title-service-3">
+               <p>APIs</p>
+               <div className="w-10 h-1 md:w-23 bg-gold"/>
+               <p className="italic">Frontends</p>
+               <div className="w-10 h-1 md:w-23 bg-gold"/>
+               <p className="italic">Scalability</p>
+            </div>
+            <div id="title-service-4">
+                <p>Databases</p>
             </div>
         </section>
     )
