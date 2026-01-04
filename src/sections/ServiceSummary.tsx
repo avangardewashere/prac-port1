c2a9ecc27a852e00804dedb3c23d12419bfb1e0b
avangardewashere <avelpanaligan@gmail.com>
2026-01-04 14:43:36 +0800

    Add the service summary component

---
+++ src/sections/ServiceSummary.tsx
@@ -0,0 +1,20 @@
+const ServiceSummary = () => {
+    return (
+        <section className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive ">
+            <div id="title-service-1">
+                <p>Architecture</p>
+            </div>
+            <div id="title-service-2" className="flex items-center justify-center gap-3 translate-x-16">
+                <p className="font-normal">Development</p>
+                <div className="w-10 h-1 md:w-32 bg-gold">
+                    <p>Deployment</p></div>
+            </div>
+            <div className="" id="title-service-3">
+                <p className="flex-items-center justify-center gap-3 -translate-x-48">
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default ServiceSummary;
