f20b6b8e0b98fddb99a013cfd18254c87127bbbf
avangardewashere <avelpanaligan@gmail.com>
2026-01-06 21:54:26 +0800

    finalized the services

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,7 +1,18 @@
+import AnimatedHeader from "../components/AnimatedHeader"
+
 const Services = () => {
+    const text = 'I build secure, high-perfomance full-stack app with the smoothUX to drive growth not headaches'
     return(
         <section id="services" className="min-h-screen bg-black rounded-t-4xl">
-            
+            <AnimatedHeader
+            subtitle={"Behind the scene, Beyond the screen"}
+            title={"Service"}
+            text={text}
+            TextColor="text-white"
+            /
+            >
         </section>
     )
 }
+
+export default Services;
