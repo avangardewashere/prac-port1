ced001cc6ed88af0bcbae807decf2f0c5e08091c
avangardewashere <avelpanaligan@gmail.com>
2026-01-08 06:21:12 +0800

    call the work component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ServiceSummary from './sections/ServiceSummary'
 import Services from './sections/Services'
 import ReactLenis from 'lenis/react'
 import About from './sections/About'
+import Works from './sections/Works'
 function App() {
   return (
     <ReactLenis className="relative w-screen min-h-screen overflow-x-auto ">
@@ -13,6 +14,7 @@ function App() {
       <ServiceSummary/>
       <Services/>
       <About/>
+      <Works/>
       <section id='home' className='min-h-screen '/>
     </ReactLenis>
   )
