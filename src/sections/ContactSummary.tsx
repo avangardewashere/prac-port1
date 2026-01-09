bd53083f27a20cbad693f73e21f9d6be7cc3e5ea
avan <avan@lottoplay.ph>
2026-01-09 17:00:10 +0800

    Add the contact summary component

--- src/sections/ContactSummary.tsx
+++ src/sections/ContactSummary.tsx
@@ -7,6 +7,18 @@ const ContactSummary = () => {
         <section ref={containerRef}
         className="flex flex-col justify-between min-h-screen gap-12 mt-16">
             {/* marquee */}
+
+            <div className="overflow-hidden font-light text-center contact-text-responsive">
+                <p>
+                    " Let's build a "
+                    <br />
+                    <span className="font-normal">memorable</span> &{" "}
+                    <span className="italic">inspiring</span>
+                    <br />
+                    web application 
+                    <span className="text-gold">togoether</span>
+                </p>
+            </div>
         </section>
     )
 }
